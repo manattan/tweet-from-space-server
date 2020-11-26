870f414518853d26ba2d3ce48f05e97dce85c7c7
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-26 14:44:47 +0900

    🎉response送信

--- ISS/views.py
+++ ISS/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+def index(request):
+    data={
+        'id': 8
+    }
+    json_str=json.dumps(data)
+    print(request)
+    print(HttpResponse(json_str,content_type="application/json, charset=utf-8"))
+    return HttpResponse(json_str)
