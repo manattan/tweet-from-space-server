1de29b8cd9b6b31197c57940c826519e05e24527
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-12-05 23:54:36 +0900

    token違うやんけ！

--- ISS/views.py
+++ ISS/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+import requests
 
 from ISS.components.getISSLocation import getISSLocation
+
+# url = 'https://api.twitter.com/1.1/direct_messages/events/new.json'
+url = "https://api.twitter.com/2/tweets/sample/stream"
 
 # Create your views here.
 def sendISSLocation(request):
@@ -11,8 +15,31 @@ def sendISSLocation(request):
     return HttpResponse(json_str)
 
 def directMessage(request):
-    token=request.headers['Authorization']
-    headers={'Authorization': 'Bearer {}'.format(token)}
-    data = json.loads(request.body)
-    message = data['message']
+    bearertoken=request.headers['Authorization']
+    headers={'Authorization': '{}'.format(bearertoken)}
+    print(headers)
+    message = json.loads(request.body)['message']
+    print(message)
+    sendData = {
+        "event": {
+            "type": "message_create",
+            "message_create": {
+                "target": {
+                    "recipient_id": "kkkdddlll3"
+                },
+                "message_data": {
+                    "text": message,
+                }
+            }
+        }
+    }
+    params = {
+        "screen_name": 'manattan_me',
+        #"count": 1,
+    }
+    # res = requests.post(url,data=sendData, headers=headers)
+    res = requests.get(url, headers=headers)
+    print(res)
+    tweets = res.json()
+    print(tweets)
     return HttpResponse()
